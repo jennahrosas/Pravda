a5e2f2f63a96a2dce0d2cdbdafb9685926afbdf9
louis8765 <30911894+louis8765@users.noreply.github.com>
2021-03-08 16:04:36 -0600

    deletes dialogue when walking away

--- state0.js
+++ state0.js
@@ -14,6 +14,7 @@ var Buildings;
 var conversation=false;
 var npc1Questions = ['Where is the pizzeria?', 'What happened?', 'Who are you?'];
 var npc1Answers = [['Just around the corner',"It's big and red you can't miss it just to the southwest"],['A busser was killed',"I don't know"],['I was just walking by', "I'm nobody"]];
+var sentence, currentLine, instructions, option1, option2, option3;
 demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
@@ -94,7 +95,7 @@ demo.state0.prototype = {
         
         
         //var text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        //this.spellOutText(0,0,800,text,30,40,'#ffffff')
+        //spellOutText(0,0,800,text,30,40,'#ffffff')
         
         //add plink sound
         plink=game.add.audio('plink');
@@ -118,6 +119,8 @@ demo.state0.prototype = {
                     detective.scale.setTo(1,1);
                     detective.body.velocity.x = speed;
                     detective.animations.play('walk',20,true);
+                    
+                    
                 }
                 else{
                     detective.animations.stop('walk');
@@ -139,7 +142,21 @@ demo.state0.prototype = {
         else{
             detective.animations.stop('walk');
             detective.frame=6;
+            detective.body.velocity.y=0
+            detective.body.velocity.x=0
         }
+        
+        if(Math.abs(detective.x-npc.x)>50 || Math.abs(detective.y-npc.y)>50){
+            if(sentence && option1 && option2 && option3 && instructions){
+                sentence.alpha=0;
+                option1.alpha=0;
+                option2.alpha=0;
+                option3.alpha=0;
+                instructions.alpha=0;
+                counter=0;
+               }
+        }
+        //console.log(detective.x,detective.y);
 
     },
     
@@ -147,41 +164,29 @@ demo.state0.prototype = {
     
 };
 //function to spell out text across the screen
-function spellOutText(width,text,fontSize,textspeed, fill, font, background){
-    //var end = false;
-    var sentence = game.add.text(0,200,'',{fontsize: fontSize+'px', fill:fill, font:font});
+function spellOutText(x,y,width,text,fontSize,speed, fill, font){
+    sentence = game.add.text(x,y,'',{fontsize: fontSize+'px', fill:fill, font:font});
+    sentence.alpha=1;
     var currentLine = game.add.text(10,10,'',{fontsize: fontSize+'px', font:font});
-    currentLine.alpha =0;    
-    var loop = game.time.events.loop(speed*.1, addChar)
+    currentLine.alpha =0;
+    var loop = game.time.events.loop(speed, addChar)
     var index=0;
     function addChar()
     {
         sentence.text+=text[index];
         currentLine.text+=text[index];
-        if(currentLine.width>width && text[index]==' '){
+        if(currentLine.width> width && text[index]==' '){
             sentence.text+='\n';
             currentLine.text=' ';
         }
-          
+
         if(index>=text.length-1){
-            //end = true
             game.time.events.remove(loop);
+            conversation=false;
             console.log('stop');
-            conversation = false;
         }
         index++;
     }
-    console.log('sup');
-    /*while (end){
-        if(game.input.keyboard.isDown(Phaser.Keyboard.E)){
-            conversation = false;
-            sentence.destroy();
-            end = false;
-            console.log('if');
-        }
-        console.log('loop');
-    }*/
-        
 }
 
 
@@ -189,26 +194,32 @@ function spellOutText(width,text,fontSize,textspeed, fill, font, background){
 var counter=0;
 var choice = 0;
 function interactionHandler(detective,npc,sound){
-    if(Math.abs(detective.x-npc.x)<80 && Math.abs(detective.y-npc.y)<80){
+    
+    if(Math.abs(detective.x-npc.x)<50 && Math.abs(detective.y-npc.y)<50){
             if(game.input.keyboard.isDown(Phaser.Keyboard.E) && counter<1){
+                console.log('interaction handler running')   
                 
                 counter++;
                 conversation=true;
                 //await sleep(3000);
                 console.log('checking');
-                var instructions = game.add.text(0,600,'',{fontsize:'20px', fill: '#ffffff'});
+                instructions = game.add.text(0,600,'',{fontsize:'20px', fill: '#ffffff'});
+                instructions.alpha=1;
                 option1 = game.add.text(0,630,'',{fontsize: '20px',fill: '#ffffff'})
                 option2 = game.add.text(0,660,'',{fontsize: '20px',fill: '#ffffff'})
                 option3 = game.add.text(0, 690, '', {fontsize: '20px',fill: '#ffffff'})
+                option1.alpha=1;
+                option2.alpha=1;
+                option3.alpha=1;
                 
                 //instructions.alpha =0;
                 //option1.alpha =0;
                 //option2.alpha =0;
                 //option3.alpha =0;
-                var loop = game.time.events.loop(speed*.1, addChar)
+                var loop = game.time.events.loop(speed*.1, addChar2)
                 var index=0;
                 var text = ['What do you want to ask?','Where is the pizzeria?  ', 'What happened?          ', 'Who are you?            ']
-                function addChar()
+                function addChar2()
                 {
                     if (index <text[0].length){
                         instructions.text+=text[0][index];
@@ -240,12 +251,17 @@ function interactionHandler(detective,npc,sound){
                 option1.inputEnabled=true;
                 option2.inputEnabled=true;
                 option3.inputEnabled=true;
-                option1.events.onInputDown.add(function(){option1.addColor('#ff0000',0); sound.play(); option2.clearColors(); option3.clearColors();});
-                option2.events.onInputDown.add(function(){option2.addColor('#ff0000',0); sound.play(); option1.clearColors(); option3.clearColors();});
-                option3.events.onInputDown.add(function(){option3.addColor('#ff0000',0); sound.play(); option1.clearColors(); option2.clearColors();});
+                option1.events.onInputDown.add(function(){option1.addColor('#ff0000',0); sound.play(); option2.clearColors(); option3.clearColors(); displayResponse(npc,1)});
+                option2.events.onInputDown.add(function(){option2.addColor('#ff0000',0); sound.play(); option1.clearColors(); option3.clearColors(); displayResponse(npc,2)});
+                option3.events.onInputDown.add(function(){option3.addColor('#ff0000',0); sound.play(); option1.clearColors(); option2.clearColors(); displayResponse(npc,3)});
             }
-    }
-        
+    }  
     
-                
+}
+
+function displayResponse(npc,option){
+    console.log('ran');
+
+    spellOutText(0,400,700,npc1Answers[option-1][Math.floor(Math.random() * 2)],30,20,'#ffffff');
+    
 }
