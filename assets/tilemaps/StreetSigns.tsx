783b53c97004996183c4ce2b69ade06eca97e402
Josh Dawe <joshdawe00@gmail.com>
2021-03-28 21:35:50 -0500

    Adding street signs

---
+++ assets/tilemaps/StreetSigns.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="StreetSigns" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <image source="StreetSignSpriteSheet.png" width="330" height="330"/>
+</tileset>
