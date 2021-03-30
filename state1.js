bb4ea1dcec291ec4c2abb3dd3ef27a371dd4996f
jennahrosas <77991479+jennahrosas@users.noreply.github.com>
2021-03-30 12:09:22 -0500

    changed some assets

--- state1.js
+++ state1.js
@@ -6,10 +6,10 @@ var velocity = 4;
 demo.state1 = function(){};
 demo.state1.prototype = {
     preload: function(){
-        game.load.image('pizzeria','assets/sprites/pizzeria.png')
-        game.load.spritesheet('pravda','assets/spritesheets/pravdawalk.png',32,64);
+        game.load.image('pizzeria','assets/sprites/pizzeria2.png',3072,512)
+        game.load.spritesheet('pravda','assets/spritesheets/pizzeriadetective1.png',256,256);
         game.load.image('cluetwo', 'assets/spritesheets/cluetwo.png', 92, 32);
-        game.load.image('pazzoli man', 'assets/spritesheets/pazzoliman.png', 28, 61);
+        game.load.spritesheet('pazzoli man', 'assets/spritesheets/pazzoliman3.png', 256, 256);
     },
     create: function(){
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -19,16 +19,19 @@ demo.state1.prototype = {
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         
         var pizzeria=game.add.sprite(0,0,'pizzeria');
-        detective=game.add.sprite(10,80,'pravda');
+        detective=game.add.sprite(10,270,'pravda');
         detective.anchor.setTo(.5);
-        detective.scale.setTo(1,1);
+        //detective.scale.setTo(1,1);
         game.physics.enable(detective);
         detective.body.collideWorldBounds=true;
         detective.enableBody=true;
         //animation for detective
-        detective.animations.add('walk',[0,1,2,3,4,5,6])
+        detective.animations.add('walk',[0,1,2,3,4])
         
-        var pazzoli=game.add.sprite(150,40,'pazzoli man');
+        game.camera.follow(detective);
+        game.camera.deadzone = new Phaser.Rectangle(100,100,500,500);
+        
+        var pazzoli=game.add.sprite(500,160,'pazzoli man');
         game.physics.enable(pazzoli);
         pazzoli.enableBody = true;
         pazzoli.physicsBodyType=Phaser.Physics.ARCADE;
@@ -37,17 +40,17 @@ demo.state1.prototype = {
         pazzoli.body.immovable=true;
         
         //npc blinking animation
-        pazzoli.animations.add('blink',[0,1,2]);
+        pazzoli.animations.add('blink',[0,1,2,3,4]);
         pazzoli.animations.play('blink',3,true);
         
-        var cluetwo = game.add.sprite(500,94,'cluetwo');
+        var cluetwo = game.add.sprite(500,390,'cluetwo');
         game.physics.enable(cluetwo);
         cluetwo.enableBody = true;
         cluetwo.physicsBodyType=Phaser.Physics.ARCADE;
         cluetwo.body.collideWorldBounds=true;
         cluetwo.animations.add('clueone',[0,1,2,3,4]);
         cluetwo.animations.play('clueone',2,true);
-        cluetwo.scale.setTo(.15);
+        cluetwo.scale.setTo(1);
         cluetwo.inputEnabled = true;
         cluetwo.events.onInputDown.add(clueClick,{clueNum:0});
 
