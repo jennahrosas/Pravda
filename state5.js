beed5dc081a68b9dccc37bc5a64e88bb0af7925c
Josh Dawe <joshdawe00@gmail.com>
2021-04-12 11:41:33 -0500

    will go to pizzeria if win

--- state5.js
+++ state5.js
@@ -126,7 +126,7 @@ demo.state5.prototype = {
     endTimer: function() {
         // Stop the timer when the delayed event triggers
         timer.stop();
-        game.state.start('state0');
+        game.state.start('state1');
     },
     
     formatTime: function(s) {
