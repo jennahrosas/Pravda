737165cf34576ee459fa8e66c46abf8dfad89d7a
louis8765 <30911894+louis8765@users.noreply.github.com>
2021-04-12 15:00:18 -0500

    fixed music

--- state0.js
+++ state0.js
@@ -136,12 +136,12 @@ demo.state0.prototype = {
         backpack.events.onInputDown.add(backpackClick, this);
         
         //add music
+        game.sound.stopAll();
         music = game.add.audio('theme');
         music.play();
         music.volume=.3;
         console.log(music.volume);
         miniMusic = game.add.audio('miniMusic');
-        miniMusic.pause();
         
         
         //var text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
